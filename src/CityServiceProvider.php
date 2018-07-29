c6e3b6326f829c3f390206766aa6fa2baa3412dd
root <root@vm332593.had.su>
2018-07-29 22:16:12 +0300

    add src

---
+++ src/CityServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vlaktion\City;
+
+use Illuminate\Support\ServiceProvider;
+
+class CityServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('vcity', function(){
+	echo 'Hello from the city package!';
+});
